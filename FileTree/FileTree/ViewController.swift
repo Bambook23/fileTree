a8918daf8fc80baa55e0707d764a1e6d5977886e
Bambook <bambook251@gmail.com>
2022-06-10 11:12:06 +0300

    create project

---
+++ FileTree/FileTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileTree
+//
+//  Created by Andrii Staroselskyi on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
