f94e70127c2d5d9cebc58a737ff44625d7d0365d
Bambook <bambook251@gmail.com>
2022-06-16 17:22:20 +0300

    create ItemAddingController

---
+++ FileTree/FileTree/Screens/ItemAddingScreen/ItemAddingController.swift
@@ -0,0 +1,61 @@
+//
+//  ItemAddingController.swift
+//  FileTree
+//
+//  Created by Andrii Staroselskyi on 16.06.2022.
+//
+
+import UIKit
+
+final class ItemAddingController: UIViewController {
+
+  private let itemAddingControllerView: ItemAddingControllerView
+  private let networkManager: NetworkManager
+
+  init(itemType: ItemType, parentUUID: String, networkManager: NetworkManager) {
+    self.itemAddingControllerView = ItemAddingControllerView()
+    self.networkManager = networkManager
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("Shoud use another init")
+  }
+
+  override func loadView() {
+    view = itemAddingControllerView
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupSubviews()
+  }
+
+}
+
+extension ItemAddingController {
+
+  private func view() -> ItemAddingControllerView {
+    view as! ItemAddingControllerView
+  }
+
+  private func setupSubviews() {
+    view().setCancelButtonAction(target: self, action: #selector(onCancelButtonTap), for: .touchUpInside)
+    view().setCreateButtonAction(target: self, action: #selector(onCreateButtonTap), for: .touchUpInside)
+  }
+
+}
+
+extension ItemAddingController {
+
+  @objc private func onCancelButtonTap() {
+    print("Adding canceled")
+  }
+
+  @objc private func onCreateButtonTap() {
+
+  }
+
+  private func createItem() {}
+
+}
